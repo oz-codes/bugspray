0a0855229f0593c6b8d54f2f8d86fbaf3152e83f
a2h <a2h@live.com.au>
2009-10-16 16:58:36 +1100

    Initial commit

--- app/settings.php
+++ app/settings.php
@@ -6,6 +6,6 @@ $mysql_database = 'bugspray';
 $mysql_prefix = 'bugspray_'; // not implemented
 
 $recaptcha_use = true;
-$recaptcha_key_public = '6Lf0sAgAAAAAALYsGtd3zWMrBLOl2QrXCEYNNFAd';
-$recaptcha_key_private = '6Lf0sAgAAAAAAIDVbnajIqw6Ju-fUaJvq9sIkK3j';
+$recaptcha_key_public = '';
+$recaptcha_key_private = '';
 ?>
