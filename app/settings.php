bae3db95d976b870836ac909f119e531997171ff
a2h <a2h@live.com.au>
2009-10-16 17:03:20 +1100

    I like tidy settings.php files <_>

--- app/settings.php
+++ app/settings.php
@@ -1,9 +1,9 @@
 <?php
-$mysql_server = 'localhost';
-$mysql_username = 'root';
-$mysql_password = '';
+$mysql_server   = 'localhost';
+$mysql_username = 'username';
+$mysql_password = 'password';
 $mysql_database = 'bugspray';
-$mysql_prefix = 'bugspray_'; // not implemented
+$mysql_prefix   = 'bugspray_'; // not implemented
 
 $recaptcha_use = true;
 $recaptcha_key_public = '';
