464190f2789b7422108336753a8a2a6684f37e2a
a2h <a2h@live.com.au>
2009-11-13 16:34:48 +1100

    Implemented some category management features, also added a placeholder for linking to projects

--- app/adm/categories.php
+++ app/adm/categories.php
@@ -1,76 +1,117 @@
 <?php
 /*
- * bugspray
- * Copyright 2009 a2h - http://a2h.uni.cc/
+ * bugspray issue tracking software
+ * Copyright (c) 2009 a2h - http://a2h.uni.cc/
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Under section 7b of the GNU General Public License you are
+ * required to preserve this notice. Additional attribution may be
+ * found in the NOTICES.txt file provided with the Program.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * http://a2h.github.com/bugspray/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 ?>
-
-<div class="ibox_error">NOT IMPLEMENTED YET</div><br />
 
 Keep your issues under order! You can have, say, "bug-low", "bug-medium", and "bug-severe", then while you're at it have "suggestion".
 
 <br />
 <br />
 
-<h3>Listing</h3>
-
-To change the colour of a category, just click the colour. It's that simple.
-
-<br />
-<br />
-
 <?php
-$result_categories = db_query("SELECT * FROM categories");
-
-$cidarr = array();
-
-while ($category = mysql_fetch_array($result_categories))
+if (!isset($_GET['s']))
 {
 	echo '
-	<div class="colorlist">
-		<input id="cat'.$category['id'].'" type="text" name="cat'.$category['id'].'" value="#'.$category['color'].'" />
-		<label for="cat'.$category['id'].'">'.$category['name'].'</label>
-		<div class="a"><a href="#">(list)</a> <a href="#">(rename)</a> <a href="#">(delete)</a></div>
-		<div class="fc"></div>
-	</div>';
-	
-	$cidarr[] = $category['id'];
-}
+	<h3>Listing</h3>
 
-echo '
-<script type="text/javascript">
-$.fn.colorPicker.defaultColors = [
-	"FF3F3F","FF7F7F","FFBFBF",
-	"FF3FFF","FF7FFF","FFBFFF",
-	"FFB23F","FFCC7F","FFE5BF",
-	"FFFF3F","FFFF7F","FFFFBF",
-	"3FBFFF","7FD4FF","BFE9FF",
-	"3FFFFF","7FFFFF","BFFFFF",
-	"7FFF3F","AAFF7F","D4FFBF",
-	"7F66FF","947FFF","C9BFFF"
-];';
+	To change the colour of a category, just click the colour. It\'s that simple.
 
-foreach ($cidarr as $cid)
-{
+	<br />
+	<br />';
+
+	$result_categories = db_query("SELECT * FROM categories");
+
+	$cidarr = array();
+
+	while ($category = mysql_fetch_array($result_categories))
+	{
+		echo '
+		<div class="colorlist">
+			<input id="cat'.$category['id'].'" type="text" name="cat'.$category['id'].'" value="#'.$category['color'].'" />
+			<label for="cat'.$category['id'].'">'.$category['name'].'</label>
+			<div class="a">
+				<a href="#">(<s>list</s>)</a>
+				<a href="javascript:void(0)" onclick="stringform(\'Rename category \\\''.str_replace("'","\'",$category['name']).'\\\'\',\''.$uri.'&amp;s=rename&amp;id='.$category['id'].'\')">(rename)</a>
+				<a href="javascript:void(0)" onclick="confirmurl(\'Delete category \\\''.str_replace("'","\'",$category['name']).'\\\'\',\''.$uri.'&amp;s=delete&amp;id='.$category['id'].'\',true);">(delete)</a>
+				<br />
+				linked projects: all <a href="#">(<s>change</s>)</a>
+			</div>
+			<div class="fc"></div>
+		</div>';
+		
+		$cidarr[] = $category['id'];
+	}
+
 	echo '
-	$("#cat'.$cid.'").colorPicker();';
-}
+	<script type="text/javascript">
+	$.fn.colorPicker.defaultColors = [
+		"FF3F3F","FF7F7F","FFBFBF",
+		"FF3FFF","FF7FFF","FFBFFF",
+		"FFB23F","FFCC7F","FFE5BF",
+		"FFFF3F","FFFF7F","FFFFBF",
+		"3FBFFF","7FD4FF","BFE9FF",
+		"3FFFFF","7FFFFF","BFFFFF",
+		"7FFF3F","AAFF7F","D4FFBF",
+		"7F66FF","947FFF","C9BFFF"
+	];';
 
-echo '
-</script>';
+	foreach ($cidarr as $cid)
+	{
+		echo '
+		$("#cat'.$cid.'").colorPicker();';
+	}
+
+	echo '
+	</script>';
+}
+else
+{
+	switch ($_GET['s'])
+	{
+		case 'add':
+			echo '<h3>Adding category</h3>';
+			/*$n = escape_smart($_POST['str']);
+			$query = db_query("INSERT INTO categories (name) VALUES ('$n')");
+			if ($query) { echo 'Added succesfully!'; } else { mysql_error(); }*/
+			echo 'Not implemented';
+			break;
+		case 'rename':
+			echo '<h3>Renaming category</h3>';
+			$n = escape_smart($_POST['str']);
+			$i = escape_smart($_GET['id']);
+			$query = db_query("UPDATE categories set name='$n' WHERE id='$i'");
+			if ($query) { echo 'Renamed succesfully!'; } else { mysql_error(); }
+			break;
+		case 'delete':
+			echo '<h3>Delete category</h3>';
+			$i = escape_smart($_GET['id']);
+			$query = db_query("DELETE FROM categories WHERE id='$i'");
+			if ($query) { echo 'Deleted succesfully!'; } else { mysql_error(); }
+			break;
+		default:
+			echo 'What?';
+			break;
+	}
+	echo '<br /><br /><a href="'.$uri2.'">Go back</a>';
+}
 ?>
