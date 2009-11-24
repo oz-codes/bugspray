76ee69df9e5b03ba4c655b2e5723eda2d13703fa
a2h <a2h@live.com.au>
2009-11-24 21:35:32 +1100

    Activity page migrated over to templating system

--- app/activity.php
+++ app/activity.php
@@ -25,75 +25,38 @@
 include("functions.php");
 $page->setType('activity');
 $page->setTitle('Activity');
-?>
-<h2>Recent activity</h2>
 
-<b>Logging has only been implemented for adding issues right now.</b><br /><br />
+$projects = db_query_toarray("SELECT id, name FROM projects");
 
-<?php
-$result_projects = db_query("SELECT * FROM projects");
-while ($project = mysql_fetch_array($result_projects))
+$i = 0;
+foreach ($projects as $project)
 {
-	echo '<h3><a href="#">'.$project['name'].'</a> <a href="#"><img src="img/feed.png" alt="RSS feed" /></a></h3>';
-	
-	$i=0;
-	$result_issues = db_query("SELECT * FROM issues WHERE project = '".$project['id']."'");
-	while ($issue = mysql_fetch_array($result_issues))
+	$result_logs = db_query("
+		SELECT issues.category as issuecat, issues.name as issuename, log_issues.when_occured as logwhen, log_issues.userid, log_issues.actiontype
+		FROM issues
+		RIGHT JOIN log_issues ON log_issues.issue = issues.id
+		WHERE project = '{$project['id']}'
+		ORDER BY log_issues.when_occured DESC
+	");
+	$j=0;
+	while ($issue = mysql_fetch_array($result_logs))
 	{				
-		$result_log_issues = mysql_query("SELECT * FROM log_issues WHERE issue = '".$issue['id']."'");
-		while ($log_issue = mysql_fetch_array($result_log_issues))
-		{
-			$log_entry[$i] = array(
-				'when_occured' => strtotime($log_issue['when']),
-				'actiontype' => $log_issue['actiontype'],
-				'userid' => $log_issue['userid'],
-				'category' => $issue['category'],
-				'name' => $issue['name']
-			);
-			$i+=1;
-		}
+		$projects[$i]['logs'][$j] = array(
+			'when_occured' => strtotime($issue['logwhen']),
+			'actiontype' => $issue['actiontype'],
+			'userid' => $issue['userid'],
+			'category' => $issue['issuecat'],
+			'name' => $issue['issuename']
+		);
+		$j+=1;
 	}
-	
-	if (isset($log_entry))
-	{
-		usort($log_entry,'logwhencmp');
-		echo '<table class="watchlisttbl" cellspacing="0">';
-		for ($i=0;$i<sizeof($log_entry);$i++)
-		{
-			$u = getunm($log_entry[$i]['userid']);
-			if ($u == $_SESSION['username'])
-				$u = '<b>You</b>';
-			else
-				$u = '<a href="profile.php?u='.$log_entry[$i]['userid'].'">'.$u.'</a>';
-
-			echo '
-				<tr>
-					<td>
-						<img src="img/act/'.getactimg($log_entry[$i]['actiontype']).'" alt="" />
-					</td>
-					<td>
-						<div class="avatar">
-							<img src="'.getav($log_entry[$i]['userid']).'" alt="" />
-						</div>
-					</td>
-					<td>
-						'.$u.'
-						
-						<span style="background:#'.getactcol($log_entry[$i]['actiontype']).'">'.getactlogdsc($log_entry[$i]['actiontype']).'</span>
-						
-						categorised under
-						
-						<a href="#">'.getcatnm($log_entry[$i]['category']).'</a>,
-						
-						called <a href="#">'.$log_entry[$i]['name'].'</a>
-					</td>
-				</tr>';
-		}
-		echo '</table>';
-	}
-	else
-	{
-		echo 'Nothing...';
-	}
+	$i+=1;
 }
+
+$page->setPage(
+	'activity.php',
+	array(
+		'projects' => $projects
+	)
+);
 ?>
---
+++ app/thm/default/activity.php
@@ -0,0 +1,45 @@
+<h2>Recent activity</h2>
+
+<b>Logging has only been implemented for adding issues right now.</b><br /><br />
+
+<?php
+foreach ($projects as $project)
+{
+	echo '
+	<section class="watchlist">
+		<h3><a href="#">'.$project['name'].'</a></h3>
+		<nav><a href="#"><img src="'.$location['images'].'/feed.png" alt="(RSS)" title="Subscribe to this feed" /></a></nav>
+		<div class="fc"></div>
+		';
+		
+	foreach ($project['logs'] as $log)
+	{
+		echo '
+		<article>
+			<div class="type">
+				<img src="'.$location['images'].'/act/'.getactimg($log['actiontype']).'" alt="" />
+			</div>
+			<div class="user">
+				'.getuinfo($log['userid'],false).'
+			</div>
+			<div class="details" style="margin:2px 0px 0px 4px;">
+				<span style="background:#'.getactcol($log['actiontype']).'">'.getactlogdsc($log['actiontype']).'</span>
+				categorised under <a href="#">'.getcatnm($log['category']).'</a>,
+				called <a href="#">'.$log['name'].'</a>
+			</div>
+			<div class="fc"></div>
+		</article>';
+	}
+		
+	echo '
+	</section>';
+}
+
+if (count($projects) == 0)
+{
+	echo '
+	<section>
+		Looks like there\'s no projects around here...
+	</section>';
+}
+?>
