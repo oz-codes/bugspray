b2f1216ceef89d704cfc5ad4049440391a03d0f1
a2h <a2h@live.com.au>
2010-05-22 17:08:49 +1000

    Added some logic into the account page

--- app/account.php
+++ app/account.php
@@ -21,16 +21,38 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// todo: move stuff into a template
+
 include("functions.php");
-$page->setType('account');
 
-$id = isset($_GET['id']) ? escape_smart($_GET['id']) : $_SESSION['uid'];
-
-$subpage = $_GET['p'];
+if (isset($_GET['id']) && $_GET['id'] != $_SESSION['uid'])
+{
+	if (is_numeric($_GET['id']))
+	{
+		if ($users->client->is_admin)
+		{
+			$id = $_GET['id'];
+		}
+		else
+		{
+			$id = false;
+		}
+	}
+}
+else
+{
+	$id = $_SESSION['uid'];
+}
 
 $profile = db_query_single("SELECT users.*, groups.name AS group_name FROM users LEFT JOIN groups ON groups.id = users.group WHERE users.id = '$id'");
 
+// bad id? non-admin? don't go through! oh, and make sure the profile exists!
+if ($id && $profile)
+{
+$page->setType('account');
 $page->setTitle($profile['username'] . '\'s account');
+
+$subpage = $_GET['p'];
 ?>
 
 <div class="imgtitle imgtitle-32">
@@ -49,3 +71,38 @@ $page->setTitle($profile['username'] . '\'s account');
 	<div class="fc"></div>
 </div>
 
+<?php
+if (!$subpage)
+{
+	$subpage = 'home';
+}
+
+switch ($subpage)
+{
+	case 'home':
+		echo '<p>This is a temporary home page for your account settings.
+		There\'s nothing to show here yet, perhaps later in mintytracker\'s life there will be. Until then, take a look at the tabs above.</p>';
+		break;
+}
+
+}
+// bad id? non-admin? non-existent profile? it am be 404 tiem
+else
+{
+$page->setType('error');
+?>
+<div class="imgtitle imgtitle-32">
+	<img class="image" src="<?php echo $location['images']; ?>/titles/error.png" alt="" />
+	<div class="text">
+		<h1>Error!</h1>
+	</div>
+	<div class="clear"></div>
+</div>
+<p>Oh dear! It looks like you either don't have permission to access this page, or it doesn't exist!</p>
+<p>If perhaps your problem is due to permissions, have a look-see if you're logged in. If you are, perhaps
+this is a restricted page.</p>
+<p>Perhaps it's not permissions? If you typed out the address to this page manually, be sure to check if your spllnig is correct!
+Otherwise, pop around to the <a href="./">home page</a> and try and see if you can find what you were looking for.</p>
+<?php
+}
+?>
