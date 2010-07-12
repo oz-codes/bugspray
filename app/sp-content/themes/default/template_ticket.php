d2f8ad185f59b3c04d038e7816c8f958340a31c7
sirmxe <warman.t.gary@gmail.com>
2010-07-11 20:57:27 -0500

    DRY: wrapping tag output into a function.

--- app/sp-content/themes/default/template_ticket.php
+++ app/sp-content/themes/default/template_ticket.php
@@ -6,12 +6,7 @@
 		</div>
 		
 		<?php
-			$tags = explode(' ', $issue['tags']); // todo: use the separate table for tags instead of one long string
-			foreach ($tags as $tag)
-			{
-				if($tag == "") { continue; }
-				echo '<span class="tag"><a href="#">' . $tag . '</a></span>';
-			}
+			echo_tags($issue['tags']);
 		?>
 		
 		<?php if ($users->client->is_admin): ?><div class="right">
