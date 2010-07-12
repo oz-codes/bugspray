3e41494b0b8c0c2ccfaac99e27ffc3fd1af0c10b
sirmxe <warman.t.gary@gmail.com>
2010-07-11 20:56:11 -0500

    Fixing display of empty tags on ticket page.

--- app/sp-content/themes/default/template_ticket.php
+++ app/sp-content/themes/default/template_ticket.php
@@ -9,6 +9,7 @@
 			$tags = explode(' ', $issue['tags']); // todo: use the separate table for tags instead of one long string
 			foreach ($tags as $tag)
 			{
+				if($tag == "") { continue; }
 				echo '<span class="tag"><a href="#">' . $tag . '</a></span>';
 			}
 		?>
